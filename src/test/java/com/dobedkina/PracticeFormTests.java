e80f56177d6b53b9ed2d72fc9f76ad5a983978a7
Дарья Обедкина <dobedkina@solit-clouds.ru>
2021-09-01 22:05:39 +0300

    fillFormTest added

---
+++ src/test/java/com/dobedkina/PracticeFormTests.java
@@ -0,0 +1,65 @@
+package com.dobedkina;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeFormTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void fillFormTest() {
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String email = "test@test.test";
+        String number = "1234567890";
+        String subject1 = "Maths";
+        String subject2 = "History";
+        String address = "Moscow, Test str., 15";
+
+        open("https://demoqa.com/automation-practice-form");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue(number);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").$(byText("May")).click();
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").$(byText("2002")).click();
+        $(".react-datepicker__day--012").click();
+        $("#subjectsInput").setValue(subject1).pressEnter();
+        $("#subjectsInput").setValue(subject2).pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("cat.jpg");
+        $("#currentAddress").setValue(address);
+        $("#state").click();
+        $("#state").$(byText("NCR")).click();
+        $("#city").click();
+        $("#city").$(byText("Delhi")).click();
+        $("#submit").click();
+        $("#submit").click();
+
+
+
+
+
+
+
+
+    }
+}
