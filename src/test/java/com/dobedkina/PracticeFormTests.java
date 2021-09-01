8d5bf5732dc3f9cb46541fd02b94ee5e2661ea4c
Дарья Обедкина <dobedkina@solit-clouds.ru>
2021-09-01 23:03:49 +0300

    added more complex checks + refactored test code (replaced text with variables)

--- src/test/java/com/dobedkina/PracticeFormTests.java
+++ src/test/java/com/dobedkina/PracticeFormTests.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
 
@@ -21,39 +20,56 @@ public class PracticeFormTests {
         String firstName = "Ivan";
         String lastName = "Ivanov";
         String email = "test@test.test";
-        String number = "1234567890";
+        String gender  = "Male";
+        String month = "May";
+        String day = "12";
+        String year = "2002";
+        String phone = "1234567890";
         String subject1 = "Maths";
         String subject2 = "History";
+        String hobby1 = "Sports";
+        String hobby2 = "Reading";
+        String hobby3 =  "Music";
         String address = "Moscow, Test str., 15";
+        String state = "NCR";
+        String city = "Delhi";
 
         open("https://demoqa.com/automation-practice-form");
 
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
-        $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue(number);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(phone);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").click();
-        $(".react-datepicker__month-select").$(byText("May")).click();
+        $(".react-datepicker__month-select").$(byText(month)).click();
         $(".react-datepicker__year-select").click();
-        $(".react-datepicker__year-select").$(byText("2002")).click();
-        $(".react-datepicker__day--012").click();
+        $(".react-datepicker__year-select").$(byText(year)).click();
+        $(byText(day)).click();
         $("#subjectsInput").setValue(subject1).pressEnter();
         $("#subjectsInput").setValue(subject2).pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#hobbiesWrapper").$(byText("Reading")).click();
-        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText(hobby1)).click();
+        $("#hobbiesWrapper").$(byText(hobby2)).click();
+        $("#hobbiesWrapper").$(byText(hobby3)).click();
         $("#uploadPicture").uploadFromClasspath("cat.jpg");
         $("#currentAddress").setValue(address);
         $("#state").click();
-        $("#state").$(byText("NCR")).click();
+        $("#state").$(byText(state)).click();
         $("#city").click();
-        $("#city").$(byText("Delhi")).click();
+        $("#city").$(byText(city)).click();
         $("#submit").click();
 
-        $(".modal-content").should(Condition.exist);
 
-
+        $x("//td[.='Student Name']/following-sibling::td").shouldHave(Condition.text(firstName + " " + lastName));
+        $x("//td[.='Student Email']/following-sibling::td").shouldHave(Condition.text(email));
+        $x("//td[.='Gender']/following-sibling::td").shouldHave(Condition.text(gender));
+        $x("//td[.='Mobile']/following-sibling::td").shouldHave(Condition.text(phone));
+        $x("//td[.='Date of Birth']/following-sibling::td").shouldHave(Condition.text(day + " " + month + "," + year));
+        $x("//td[.='Subjects']/following-sibling::td").shouldHave(Condition.text(subject1 + ", " + subject2));
+        $x("//td[.='Hobbies']/following-sibling::td").shouldHave(Condition.text( hobby1 + ", " + hobby2 + ", " +hobby3));
+        $x("//td[.='Picture']/following-sibling::td").shouldHave(Condition.text("cat.jpg"));
+        $x("//td[.='Address']/following-sibling::td").shouldHave(Condition.text(address));
+        $x("//td[.='State and City']/following-sibling::td").shouldHave(Condition.text(state + " " + city));
     }
 }
