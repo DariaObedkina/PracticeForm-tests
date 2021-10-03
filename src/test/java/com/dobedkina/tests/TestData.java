1a00c37844cd657bf2ae5a4fbba1182a2dce7338
Дарья Обедкина <dobedkina@solit-clouds.ru>
2021-10-03 13:12:17 +0300

    Removed comments from TestData

--- src/test/java/com/dobedkina/tests/TestData.java
+++ src/test/java/com/dobedkina/tests/TestData.java
@@ -69,8 +69,10 @@ public class TestData {
         this.address = faker.address().fullAddress();
     }
 
-    public Faker getFaker() {
-        return faker;
+    static private String randomGender() {
+        List<String> genders = Arrays.asList("Male", "Female", "Other");
+        int randNumber = (int) (Math.random() * 3);
+        return genders.get(randNumber);
     }
 
     public String getGender() {
@@ -139,31 +141,6 @@ public class TestData {
 
     public String getAddress() {
         return address;
-    }
-//    public static String gender = randomGender();
-//    public static String month = "May";
-//    public static String day = "12";
-//    public static String year = "2002";
-//    public static String subject1 = "Maths";
-//    public static String subject2 = "History";
-//    public static String hobby1 = "Sports";
-//    public static String hobby2 = "Reading";
-//    public static String hobby3 = "Music";
-//    public static String picture = "cat.jpg";
-//    public static String state = "NCR";
-//    public static String city = "Delhi";
-
-//    static Faker faker = new Faker();
-//    public static String firstName = faker.name().firstName();
-//    public static String lastName = faker.name().lastName();
-//    public static String email = faker.internet().emailAddress();
-//    public static String phone = faker.number().digits(10);
-//    public static String address = faker.address().fullAddress();
-
-    static private String randomGender() {
-        List<String> genders = Arrays.asList("Male", "Female", "Other");
-        int randNumber = (int) (Math.random() * 3);
-        return genders.get(randNumber);
     }
 
 
