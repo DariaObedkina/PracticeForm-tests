47752bf8eee8fc5eea1bbdf5512e4b2d5abe6fd1
Дарья Обедкина <dobedkina@solit-clouds.ru>
2021-10-03 12:41:22 +0300

    Refactoring

--- src/test/java/com/dobedkina/pages/components/Calendar.java
+++ src/test/java/com/dobedkina/pages/components/Calendar.java
@@ -1,7 +1,5 @@
 package com.dobedkina.pages.components;
 
-import com.dobedkina.tests.TestData;
-
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
@@ -9,9 +7,9 @@ public class Calendar {
     public void setDate(String year, String month, String day) {
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").click();
-        $(".react-datepicker__month-select").$(byText(TestData.month)).click();
+        $(".react-datepicker__month-select").$(byText(month)).click();
         $(".react-datepicker__year-select").click();
-        $(".react-datepicker__year-select").$(byText(TestData.year)).click();
+        $(".react-datepicker__year-select").$(byText(year)).click();
         $(String.format(".react-datepicker__day--0%s:not(.react-datepicker__day--outside-month)", day)).click();
     }
 }
